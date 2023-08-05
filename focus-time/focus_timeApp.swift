5664cc3042530ff9c82546da612b24fb61526f6d
Alberto Benatti <albi.benni8@gmail.com>
2023-08-05 15:42:10 +0200

    Initial Commit

---
+++ focus-time/focus_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  focus_timeApp.swift
+//  focus-time
+//
+//  Created by Alberto Benatti on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct focus_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
